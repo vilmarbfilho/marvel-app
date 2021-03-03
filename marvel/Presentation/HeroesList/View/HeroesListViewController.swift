4b5d6cf84a6e275ad4f15d67f81f6455e4f993d9
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-03-03 18:00:52 -0300

    Refactor structure folders

---
+++ marvel/Presentation/HeroesList/View/HeroesListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroesListViewController.swift
+//  marvel
+//
+//  Created by Vilmar Bispo Filho on 3/3/21.
+//
+
+import UIKit
+
+class HeroesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
