7be54dbb733ab024c5216ba09982ffe4e56c257a
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-03-05 10:06:02 -0300

    Parcial implementation of hero table view cell

---
+++ marvel/Presentation/HeroesList/View/HeroTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  HeroTableViewCell.swift
+//  marvel
+//
+//  Created by Vilmar Bispo Filho on 3/4/21.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+class HeroTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var nameCell: UILabel!
+    @IBOutlet weak var descriptionCell: UILabel!
+ 
+    func configureCell(heroModel: HeroModel) {
+        nameCell.text = heroModel.name
+        descriptionCell.text = heroModel.description
+        
+        let url = URL(string: heroModel.imageURL)
+        imageCell.kf.setImage(with: url)
+    }
+}
--- marvel/Presentation/HeroesList/View/HeroesListViewController.swift
+++ marvel/Presentation/HeroesList/View/HeroesListViewController.swift
@@ -7,21 +7,33 @@
 
 import UIKit
 
-class HeroesListViewController: UIViewController, UITableViewDataSource {
+class HeroesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var heroes: Array<HeroModel> = [
+        HeroModel(name: "Captain America", description: "The First Avenger", imageURL: "https://i.pinimg.com/originals/97/bf/27/97bf27becd0df4ff387b882572925416.jpg"),
+        HeroModel(name: "Iron Man", description: "Genius. Billionaire. Philanthropist.", imageURL: "https://conteudo.imguol.com.br/c/entretenimento/96/2020/08/07/iron-man-1596813808466_v2_450x337.jpg")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return heroes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "HeroCellIdentifier") as? HeroTableViewCell {
+            cell.configureCell(heroModel: heroes[indexPath.row])
+            return cell
+        }
+
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
