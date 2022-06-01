86ebf55a5eaa0cc3b77b0501be3b3f3dca04ab4b
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-05-31 21:48:51 -0300

    Create StringExtensions on data group

--- ios/marvel/Data/MarvelService.swift
+++ ios/marvel/Data/MarvelService.swift
@@ -10,4 +10,19 @@ import Alamofire
 
 class MarvelService {
     
+    func getComics() {
+        let currentTimeMillis = Date().currentTimeMillis()
+        let ts = String(currentTimeMillis)
+        
+        let publicKey = "marvel public Key"
+        let privateKey = "marvel public Key"
+        let apiKey = publicKey
+        
+        
+        let hash = (ts + privateKey + publicKey).md5
+        
+        AF.request("http://gateway.marvel.com/v1/public/comics?ts=\(ts)&apikey=\(apiKey)&hash=\(hash)").response { response in
+            debugPrint(response)
+        }
+    }
 }
