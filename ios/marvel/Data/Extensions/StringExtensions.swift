0725f3e0df5f760ace5bb141f5c1ce6221e31e83
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-05-31 21:48:24 -0300

    Create StringExtensions on data group

---
+++ ios/marvel/Data/Extensions/StringExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  StringExtensions.swift
+//  marvel
+//
+//  Created by Vilmar Bispo Filho on 31/05/22.
+//
+
+import Foundation
+import CommonCrypto
+
+extension String {
+    var md5: String {
+        let data = Data(self.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+            return hash
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
