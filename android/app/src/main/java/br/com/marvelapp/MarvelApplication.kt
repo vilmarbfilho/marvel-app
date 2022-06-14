edba0be47dc06850aac021f4c44e6bbcbc6622a7
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-06-14 09:39:18 -0300

    Create MarvelApplication and setup koin start

---
+++ android/app/src/main/java/br/com/marvelapp/MarvelApplication.kt
@@ -0,0 +1,21 @@
+package br.com.marvelapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MarvelApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@MarvelApplication)
+        }
+    }
+}
