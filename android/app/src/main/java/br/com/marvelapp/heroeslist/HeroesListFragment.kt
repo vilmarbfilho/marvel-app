3f00c8338eae167dd602dac07674f1d6d6ef159a
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-06-21 20:52:26 -0300

    Some adjustments

--- android/app/src/main/java/br/com/marvelapp/heroeslist/HeroesListFragment.kt
+++ android/app/src/main/java/br/com/marvelapp/heroeslist/HeroesListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.marvelapp.databinding.FragmentHeroesListBinding
+import br.com.marvelapp.heroeslist.adapter.HeroesAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HeroesListFragment: Fragment() {
@@ -13,8 +15,22 @@ class HeroesListFragment: Fragment() {
     private lateinit var binding: FragmentHeroesListBinding
     private val viewModel: HeroesListViewModel by viewModel()
 
+    private val adapter = HeroesAdapter()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHeroesListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        with(binding.rvHeroesList) {
+            layoutManager = LinearLayoutManager(requireContext())
+
+        }
     }
 }
