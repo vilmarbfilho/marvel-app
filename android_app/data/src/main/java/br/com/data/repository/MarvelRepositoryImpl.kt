7c37aed99ade413f39a1c6a22f9b4f4ae00a45e7
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-08-31 22:12:36 -0300

    Finish Marvel repositoy implementation

--- android_app/data/src/main/java/br/com/data/repository/MarvelRepositoryImpl.kt
+++ android_app/data/src/main/java/br/com/data/repository/MarvelRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package br.com.data.repository
 
+import br.com.data.mapper.toCharacters
 import br.com.data.service.MarvelService
 import br.com.marvelapp.entity.Characters
 import br.com.marvelapp.repository.MarvelRepository
 
-class MarvelRepositoryImpl(private val service: MarvelService): MarvelRepository {
+class MarvelRepositoryImpl(private val service: MarvelService) : MarvelRepository {
     override suspend fun getCharacters(): List<Characters> {
-        TODO("Not yet implemented")
+        val data = service.getCharacters().data
+        return data.results.map {
+            it.toCharacters()
+        }
     }
 }
