cb38a148a44c34b58ba16d1a3d15d400b20b90b6
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-09-01 15:54:35 -0300

    Setup Glide to load image

--- android_app/app/src/main/java/br/com/marvelapp/characterlist/adapter/CharacterViewHolder.kt
+++ android_app/app/src/main/java/br/com/marvelapp/characterlist/adapter/CharacterViewHolder.kt
@@ -3,6 +3,7 @@ package br.com.marvelapp.characterlist.adapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.marvelapp.characterlist.model.CharacterUi
 import br.com.marvelapp.databinding.ItemCharacterBinding
+import com.bumptech.glide.Glide
 
 class CharacterViewHolder(
     private val view: ItemCharacterBinding
@@ -11,5 +12,9 @@ class CharacterViewHolder(
     fun bind(hero: CharacterUi) {
         view.tvNameHeroItem.text = hero.name
         view.tvDescriptionHeroItem.text = hero.description
+
+        Glide.with(view.root.context)
+            .load(hero.imageUrl)
+            .into(view.ivHeroItem)
     }
 }
