4109402dfb370c0290fed1c873e094afce8ac0b6
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2023-06-24 19:14:00 -0300

    update project to new versions of plugins

---
+++ android_app/app/src/main/java/br/com/marvelapp/router/AppRouter.kt
@@ -0,0 +1,11 @@
+//package br.com.marvelapp.router
+//
+//import androidx.navigation.NavController
+//
+//class AppRouter(private val navController: NavController) {
+//
+//    fun openDetail() {
+//        FlutterActivity
+////       val flutterEngine = FlutterEngine()
+//    }
+//}
